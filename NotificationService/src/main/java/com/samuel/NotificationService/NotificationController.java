c0c7f6d7fb00e7c5422af00cb508f6a86e563610
samuelgbenga <samuelgbenga972@gmail.com>
2024-11-29 07:14:04 +0100

    webclient did not work for some reason

--- NotificationService/src/main/java/com/samuel/NotificationService/NotificationController.java
+++ NotificationService/src/main/java/com/samuel/NotificationService/NotificationController.java
@@ -1,7 +1,10 @@
 package com.samuel.NotificationService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Map;
@@ -11,14 +14,15 @@ import java.util.Map;
 public class NotificationController {
     private final WebClient webClient;
 
+    @Autowired
     public NotificationController(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl("http://USER-SERVICE").build();
+        this.webClient = webClientBuilder.build();
     }
 
     @GetMapping("/send/{userId}")
     public String sendNotification(@PathVariable String userId) {
         Map<String, String> user = webClient.get()
-                .uri("/users/" + userId)
+                .uri("http://USER-SERVICE/users/" + userId)
                 .retrieve()
                 .bodyToMono(new ParameterizedTypeReference<Map<String, String>>() {})
                 .block();
@@ -26,4 +30,22 @@ public class NotificationController {
         return "Notification sent to " + user.get("name") + " (" + user.get("email") + ")";
         //return "Hello world";
     }
+
+
+//    private final RestTemplate restTemplate;
+//
+//    @Autowired
+//    public NotificationController(RestTemplate restTemplate) {
+//        this.restTemplate = restTemplate;
+//    }
+//
+//    @GetMapping("/send/{userId}")
+//    public String sendNotification(@PathVariable String userId) {
+//        // Make a REST call to fetch user details
+//        String url = "http://USER-SERVICE/users/" + userId;
+//        Map<String, String> user = restTemplate.getForObject(url, Map.class);
+//
+//        // Return the notification message
+//        return "Notification sent to " + user.get("name") + " (" + user.get("email") + ")";
+//    }
 }
--- NotificationService/src/main/java/com/samuel/NotificationService/NotificationServiceApplication.java
+++ NotificationService/src/main/java/com/samuel/NotificationService/NotificationServiceApplication.java
@@ -2,6 +2,10 @@ package com.samuel.NotificationService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class NotificationServiceApplication {
@@ -9,5 +13,14 @@ public class NotificationServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
+
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+
 
 }
